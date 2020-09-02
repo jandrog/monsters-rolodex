7b76f1d257e51c6e3d7aaaf3a1334221cd91b748
Alejandro Garcia <jandrogarciat384@gmail.com>
2020-09-02 01:32:13 -0500

    Updates

---
+++ precache-manifest.d51531efb6ff960966f2c4a35e29f403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f1863138f7edc2c872769adb5c67c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7c49c2fa1949f0abad60",
+    "url": "/monsters-rolodex/static/css/main.982dbe6d.chunk.css"
+  },
+  {
+    "revision": "bccc755da70b84b66ade",
+    "url": "/monsters-rolodex/static/js/2.8e19865c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8e19865c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c49c2fa1949f0abad60",
+    "url": "/monsters-rolodex/static/js/main.70777c2b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
